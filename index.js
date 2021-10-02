27a68e0625d5ae0cfe6d1d2fddf9383a2771d13d
Jzapata <jhonny.z1022@gmail.com>
2021-10-02 02:24:33 -0500

    mi primera peticion GET

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hola mundo !!')
+// llamamos el paquete de express
+const express = require('express');
+
+/** Crear Servidor Express */
+const app = express();
+
+/**Rutas */
+// app.get('/', (req, resp)=> {
+//     resp.json({
+//         ok: true
+//     });
+// })
+
+
+
+/** Escuchar las peticiones */
+
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en el puerto 4000`)
+})
